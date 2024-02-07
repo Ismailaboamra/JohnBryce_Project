24d6084c7ad9a15b974950655a8f02c17b85a9c4
Ismailaboamra <ismail.amra12@gmail.com>
2024-02-08 01:26:48 +0200

    Level1->part3->ConnectionPool

--- src/Tester.java
+++ src/Tester.java
@@ -1,8 +1,13 @@
+import CLS.ConnectionPool;
+
+import java.sql.Connection;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Tester {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws InterruptedException {
+        Connection connection = ConnectionPool.getInstance().getConnection();
+
 //
         }
     }
