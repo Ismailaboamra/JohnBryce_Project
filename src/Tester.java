fe4865d7d9bcbeba48349149ede7f32cc31d6eac
Ismailaboamra <ismail.amra12@gmail.com>
2024-02-07 20:37:45 +0200

    test!

--- src/Tester.java
+++ src/Tester.java
@@ -2,6 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Tester {
     public static void main(String[] args) {
+        System.out.println("Hello World");
+
 //
         }
     }
