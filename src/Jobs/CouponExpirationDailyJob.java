2d138dc0aab091a31ed41b313b743d94d3ba4e7c
Ismailaboamra <ismail.amra12@gmail.com>
2024-02-15 16:58:40 +0200

    level1->part7->JOB

---
+++ src/Jobs/CouponExpirationDailyJob.java
@@ -0,0 +1,22 @@
+package Jobs;
+
+import DAO.CouponsDAO;
+import DBDAO.CouponsDBDAO;
+
+public class CouponExpirationDailyJob implements Runnable{
+
+    private CouponsDBDAO couponsDAO;
+
+    public CouponExpirationDailyJob() {
+    }
+
+
+    private boolean quit;
+    @Override
+    public void run() {
+
+    }
+    public void stop(){
+
+    }
+}
